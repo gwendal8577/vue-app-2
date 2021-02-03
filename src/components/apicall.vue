552fabc56f4aa5bf40a401b222df618ae5bc3dea
gwendal8577 <gwendal.mains@gmail.com>
2021-02-03 10:33:42 +0100

    v0

---
+++ src/components/apicall.vue
@@ -0,0 +1,38 @@
+<template>
+  $END$
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.component('apiCall', {
+  data: function () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+        .get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+        .then(response => (this.info = response))
+  }
+})
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+        .get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+        .then(response => (this.info = response))
+  }
+})
+</script>
+
+<style scoped>
+</style>
