c628222d5c72b59500589d2c06201c74d1154d23
gwendal8577 <gwendal.mains@gmail.com>
2021-02-08 18:25:48 +0100

    v1

---
+++ docs/precache-manifest.cb980b08bf12ca92ea4dd335d56c6ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0afc7f545ccd549979",
+    "url": "/vue-app-2/css/app.88ae266a.css"
+  },
+  {
+    "revision": "e598ab6361b7f54d48f84e31a607ba6e",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "3c0afc7f545ccd549979",
+    "url": "/vue-app-2/js/app.418f7d59.js"
+  },
+  {
+    "revision": "b76c83ca6f0d9ae3d017",
+    "url": "/vue-app-2/js/chunk-vendors.33fafc23.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
